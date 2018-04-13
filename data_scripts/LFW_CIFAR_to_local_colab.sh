a29828c9230a6fe2f6c58f268bcdddea58174856
josh <josh.m.joseph@gmail.com>
2018-04-13 06:33:55 -0400

    sdfa

--- data_scripts/LFW_CIFAR_to_local_colab.sh
+++ data_scripts/LFW_CIFAR_to_local_colab.sh
@@ -4,22 +4,22 @@ echo -e "\n *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*"
 echo -e "\n Launching script."
 echo -e "\n *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*"
 echo -e "\n Executing download_lfw_raw.sh"
-. ./data_scripts/download_lfw_raw.sh
+. ./test_colab/data_scripts/download_lfw_raw.sh
 
 echo -e "\n *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*"
 echo -e "\n Executing flatten_lfw_raw.sh"
 echo -e "\n *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*"
-. ./data_scripts/flatten_lfw_raw.sh
+. ./test_colab/data_scripts/flatten_lfw_raw.sh
 
 echo -e "\n *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*"
 echo -e "\n Executing download_cifar-10.sh"
 echo -e "\n *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*"
-. ./data_scripts/download_cifar-10.sh
+. ./test_colab/data_scripts/download_cifar-10.sh
 
 echo -e "\n *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*"
 echo -e "\n Executing unpickle_cifar-10.py"
 echo -e "\n *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*"
-python ./data_scripts/unpickle_cifar-10.py
+python ./test_colab/data_scripts/unpickle_cifar-10.py
 
 echo -e "\n *~*~*~*~*~*~*~*~*~*"
 echo -e "\n Cleaning up ./raw_data"
